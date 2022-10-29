7e46f1afa23a6eb6739e54c9487bbd83230da90e
leivahm <leivahmarcelo@gmail.com>
2022-10-28 23:35:43 -0300

    improving calculator

--- calculator.ts
+++ calculator.ts
@@ -1,10 +1,16 @@
-const calculator = (a: number, b: number, op: 'multiply' | 'add' | 'divide') => {
-    if (op == 'multiply') return a * b
+type Operation = 'multiply' | 'add' | 'divide'
+type Result = number | string
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+    if (op === 'multiply') return a * b
     if (op === 'add') return a + b
+
     if (op == 'divide') {
-        if (b===0) return 'can\t divide by 0! Sorry!'
+        if (b === 0) return 'can\t divide by 0! Sorry!'
         return a/b
     }
+
+    throw new Error('Operation is not valid')
 }
 
 console.log(calculator(1, 3, 'add'))
