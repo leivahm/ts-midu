0579ddec45e04acd5ad2a6d4ae5da82874352052
leivahm <leivahmarcelo@gmail.com>
2022-10-29 12:52:37 -0300

    calculator finished

--- calculator.ts
+++ calculator.ts
@@ -6,12 +6,21 @@ const calculator = (a: number, b: number, op: Operation): Result => {
     if (op === 'add') return a + b
 
     if (op == 'divide') {
-        if (b === 0) return 'can\t divide by 0! Sorry!'
+        if (b === 0) return 'cannot divide by 0! Sorry!'
         return a/b
     }
 
     throw new Error('Operation is not valid')
 }
 
-console.log(calculator(1, 3, 'add'))
-console.log(calculator(1, 3, 'divide'))
+console.log(process.argv)
+
+/*try{
+    console.log(calculator(1, 5, 'divide'))
+    console.log(calculator(1, 0, 'divide'))
+} catch (e) {
+    console.log('Something went wrong ', e)
+}*/
+
+/*console.log(calculator(1, 3, 'add'))
+console.log(calculator(1, 3, 'divide'))*/
--- multiplier.ts
+++ multiplier.ts
@@ -2,4 +2,11 @@ const multiplicator = (a: number, b: number, prinText: string) => {
     console.log(prinText, a * b)
 }
 
-multiplicator(4, 8, 'Multiplied numbers 4 and 8, the result is: ')
+// multiplicator(4, 8, 'Multiplied numbers 4 and 8, the result is: ')
+
+console.log(process.argv)
+
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
+multiplicator(a, b, `Multiplied ${a} and ${b} and the result is: `)
