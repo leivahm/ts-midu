203bafef92ec865a7ceda7befabdf7a81d86e4ca
leivahm <leivahmarcelo@gmail.com>
2022-10-29 12:55:10 -0300

    details

--- multiplier.ts
+++ multiplier.ts
@@ -4,9 +4,9 @@ const multiplicator = (a: number, b: number, prinText: string) => {
 
 // multiplicator(4, 8, 'Multiplied numbers 4 and 8, the result is: ')
 
-console.log(process.argv)
+console.log(process.argv)  // add in first
 
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
+const a: number = Number(process.argv[2]) // add in first
+const b: number = Number(process.argv[3]) // add in first
 
 multiplicator(a, b, `Multiplied ${a} and ${b} and the result is: `)
